33d3c36d5ad87db005f4ebaf97b73be526ddc943
UniversessH <universess@qq.com>
2021-12-15 18:32:35 +0800

    :construction: feat: 添加 anime 相关的 hook, 引入部分资源

--- components/Accept.tsx
+++ components/Accept.tsx
@@ -1,12 +1,27 @@
-import React from "react";
-// import { motion, useMotionValue, useTransform } from "framer-motion";
+import React, { useRef, useEffect } from "react";
+import Image from "next/image";
+import { useAnime } from "../lib/hooks/useAnime";
+import accpet_img from "../public/img/accept.svg"
+import test from "../public/img/test.png"
 import styles from "./universal.module.css"
 
+
 const Accpet: React.FC = () => {
-  
+  const { animateTargetRef, animationRef } = useAnime({
+    translateY: '20px',
+    duration: 600,
+    loop: true,
+    direction: 'alternate',
+    easing: 'easeInOutSine'
+  })
+
   return (
     <div>
       <div className={styles.StatusIconArea}>
+        {/* <Image src="/img/accept.svg" width="300" height="300" layout="contain"/>
+         */}
+         <img ref={animateTargetRef} src="/img/accept.svg" style={{width:"60vw"}}></img>
+        {/* <div ref={animateTargetRef} style={{ borderRadius: "100%", width: "20px", height: "20px", backgroundColor: "green" }} /> */}
       </div>
       <div className={styles.TextArea}>
         <h3 style={{ color: "green" }}>扫码成功，请入场 🎉</h3>
