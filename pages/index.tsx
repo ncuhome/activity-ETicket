c7f284bed0aef0bd615d0cf9a4d42db243bb2c84
Kuss <2360314753@qq.com>
2021-12-19 14:25:30 +0800

    :bug: fix: token checkin request

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,9 @@ import axios from "axios";
 import { useLogin } from '../lib/hooks/useLogin'
 import { useSafeArea } from '../lib/hooks/useSafeArea'
 import { useCheckTicket } from "../lib/hooks/useCheckTicket";
-import { useInfoState } from '../store/index'
 import styles from "./index.module.css"
 import Pending from "../components/Pending";
+import { dataModule } from "mincu-react";
 
 if (process.env.NODE_ENV === 'development') {
   import('mincu-debug').then(({ default: debugModule }) => {
@@ -14,45 +14,31 @@ if (process.env.NODE_ENV === 'development') {
   })
 } //mincud引入代码
 
+const getParam = (key: string) => {
+  const url = new URL(location.href)
+  return url.searchParams.get(key)
+}
+
 const TicketPage: React.FC = () => {
   const [statusCode, setStatusCode] = useState(999)
-  const [token, setToken] = useState("")
   const { top } = useSafeArea()
   const { isReady } = useLogin()
-  const studentID = useInfoState((state) => state.studentID) //获取学号
 
-  //获取url的token
-  const getToken = useEffect(() => {
-    const url = location.href
-    // const url = "https://activity-et-icket.vercel.app/?token=979007"
-    const param = getURLParam(url);
-    console.log(param)
-    setToken(param.token);
-  }, [token])
+  useEffect(() => {
+    if (isReady) {
+      const token = getParam('token')
+      const id = dataModule.userInfo.profile.entireProfile.base_info.xh
+      const ticketURL = "https://qrcode-eticket.vercel.app/api/checkin?token=" + token + "&id=" + id;
+      axios.get(ticketURL)
+        .then((res) => {
+          setStatusCode(res.data.code)
+        })
+        .catch((err) => {
+          setStatusCode(err.response.data.code)
+        })
+    }
 
-  //解析urlParam
-  const getURLParam = (url) => {
-    const _url = url || window.location.href;
-    const _urlParams = _url.match(/([?&])(.+?=[^&]+)/igm);
-    return _urlParams ? _urlParams.reduce((a, b) => {
-      const value = b.slice(1).split('=');
-      a[value[0]] = value[1]
-      return a;
-    }, {}) : {};
-  }
-
-  //发送请求验证
-  const sendTicket = useEffect(() => {
-    const ticketURL = "https://qrcode-eticket.vercel.app/api/checkin?token=" + token + "&id=" + studentID;
-    axios.get(ticketURL)
-      .then((res) => {
-        setStatusCode(res.data.code)
-      })
-      .catch((err) => {
-        setStatusCode(err.response.data.code)
-      })
-    return
-  }, [token])
+  }, [isReady])
 
   const StatusComponent = useCheckTicket(statusCode);
 
